32c763a385ab275239049df74f068a178050d4e5
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2023-03-12 17:02:18 -0700

    Style(nvim): Fix formatting

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -2,6 +2,6 @@
 ---   Keybindings
 -----------------------
 -- Switch Window | Note - This breaks Ctrl-i (Jumplist)
-vim.keymap.set('n', '<Tab>',     '<C-w><C-w>',  { silent = true })
-vim.keymap.set('n', '<C-Left>',  ':bprev<CR>',  { silent = true })
+vim.keymap.set('n', '<Tab>',     '<C-w><C-w>', { silent = true })
+vim.keymap.set('n', '<C-Left>',  ':bprev<CR>', { silent = true })
 vim.keymap.set('n', '<C-Right>', ':bnext<CR>', { silent = true })
