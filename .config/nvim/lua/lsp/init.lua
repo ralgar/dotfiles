02ee524fee5e08626e2ac1f7594a7fdf37856019
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2023-03-20 19:37:10 -0700

    (nvim): Add space-e keybind for floating diagnostics

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -2,3 +2,5 @@ use('neovim/nvim-lspconfig')
 require('lsp/mason')
 require('lsp/null-ls')
 require('lsp/completion')
+
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, { noremap = true, silent = true })
