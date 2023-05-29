9fd182f4d943c11900a3024ac524b535aed5d5e2
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2023-05-29 13:27:04 -0700

    fix(nvim): update mason-null-ls plugin config

--- .config/nvim/lua/lsp/null-ls.lua
+++ .config/nvim/lua/lsp/null-ls.lua
@@ -13,19 +13,6 @@ use({
             -- Automatically configure packages on install
             automatic_setup = true,
         })
-
-        require 'mason-null-ls'.setup_handlers {
-            function(source_name, methods)
-                -- all sources with no handler get passed here
-
-                -- To keep the original functionality of `automatic_setup = true`,
-                -- please add the below.
-                require("mason-null-ls.automatic_setup")(source_name, methods)
-            end,
-            stylua = function(source_name, methods)
-                null_ls.register(null_ls.builtins.formatting.stylua)
-            end,
-        }
     end
 })
 user.flush()
