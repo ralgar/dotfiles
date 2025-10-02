fc735b58556a1e5190bc3cdd81db8b9ab7763dff
Ryan Algar <me@ralgar.dev>
2025-10-02 12:20:16 -0700

    feat(nvim): add keybinds to yank to system clipboard

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -10,3 +10,7 @@ vim.keymap.set('n', '<C-Right>', ':bnext<CR>',          { noremap = true, silent
 -- Toggle NeoTree and SymbolsOutline
 vim.keymap.set('n', '<S-Tab>',   ':NeoTreeShow<CR>',    { noremap = true, silent = true })
 vim.keymap.set('n', '|',         ':SymbolsOutline<CR>', { noremap = true, silent = true })
+
+-- Yank selection or current line to clipboard
+vim.keymap.set("v", "<leader>y", '"+y',  { desc = "Yank selection to clipboard" })
+vim.keymap.set("n", "<leader>y", '"+yy', { desc = "Yank line to clipboard" })
